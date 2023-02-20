20bfdceafa6e6ae34673338ef442b704da7ae24f
Allen Lin <ht016746@gmail.com>
2023-02-20 21:58:09 +0700

    chore: clean up repo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '~/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { appWithTranslation } from 'next-i18next';
+import CssBaseline from '@mui/material/CssBaseline';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+      <CssBaseline>
+        <Component {...pageProps} />
+      </CssBaseline>
+    </>
+  );
 }
+
+export default appWithTranslation(App);
