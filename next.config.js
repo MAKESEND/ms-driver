b2055f6dc9a5c0dffa05a188ed20a52519359ba1
Allen Lin <ht016746@gmail.com>
2023-02-21 07:26:42 +0700

    feat: add app version and health check endpoint

--- next.config.js
+++ next.config.js
@@ -1,14 +1,31 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config');
+const { version } = require('./package.json');
 
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+
+  i18n,
   images: {
     domains: ['flagcdn.com', 'images.unsplash.com', 'res.cloudinary.com'],
   },
+  publicRuntimeConfig: {
+    version,
+  },
   reactStrictMode: true,
-  i18n,
-  experimental: {
-    appDir: true,
+  async rewrites() {
+    return [
+      {
+        source: '/version',
+        destination: '/api/version',
+      },
+      {
+        source: '/health',
+        destination: '/api/health',
+      },
+    ];
   },
 };
 
