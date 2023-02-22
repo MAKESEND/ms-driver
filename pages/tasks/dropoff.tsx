bec26a3eec585af823d09e21f9c218ecd0a31464
Allen Lin <ht016746@gmail.com>
2023-02-22 23:06:56 +0700

    fix: bundle i18n files

--- pages/tasks/dropoff.tsx
+++ pages/tasks/dropoff.tsx
@@ -1,7 +1,17 @@
 import { NextSeo } from 'next-seo';
+import type { GetStaticProps } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import type { NextPageWithLayout } from '~/pages/_app';
 import DrawerLayout from '~/components/layouts/drawer-layout';
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(locale && (await serverSideTranslations(locale, ['common']))),
+    },
+  };
+};
 
 const DropoffPage: NextPageWithLayout = () => {
   return (
