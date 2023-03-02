c4896614ac1ee0ff166f1270cc0448b2c11c13ad
Allen Lin <ht016746@gmail.com>
2023-03-02 21:56:46 +0700

    chore: redirect user out from protected route

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,14 +1,19 @@
 import { NextSeo } from 'next-seo';
-import type { GetStaticProps } from 'next';
+import type { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import type { NextPageWithLayout } from '~/pages/_app';
 import DrawerLayout from '~/components/layouts/drawer-layout';
+import { inAppLinks } from '~/constants/side-nav-links';
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   return {
     props: {
       ...(locale && (await serverSideTranslations(locale, ['common']))),
+    },
+    redirect: {
+      destination: inAppLinks.dashboard?.href!,
+      permenant: false,
     },
   };
 };
