39dcd3a5eb51f9d546d534785bf9bdafe6d476da
Allen Lin <ht016746@gmail.com>
2023-03-02 22:11:41 +0700

    feat: styling dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,12 @@
 import { NextSeo } from 'next-seo';
+import NextImage from 'next/legacy/image';
 import type { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { Box, Stack } from '@mui/material';
 
 import type { NextPageWithLayout } from '~/pages/_app';
 import DrawerLayout from '~/components/layouts/drawer-layout';
+import { inAppImages } from '~/constants/image-list';
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
@@ -17,7 +20,11 @@ const DashboardPage: NextPageWithLayout = () => {
   return (
     <>
       <NextSeo title='Dashboard' />
-      <div>Dashboard</div>
+      <Stack justifyContent='center' flexGrow={1}>
+        <Box position='relative' width='60%' sx={{ mx: 'auto' }}>
+          <NextImage layout='responsive' {...inAppImages.MSLogo} />
+        </Box>
+      </Stack>
     </>
   );
 };
