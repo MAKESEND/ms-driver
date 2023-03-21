c013c7e82360d65e979ca120df86f5dcc9d437c2
Allen Lin <ht016746@gmail.com>
2023-03-21 22:47:35 +0700

    perf: optimize dashboard page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,12 @@
-import { NextSeo } from 'next-seo';
-import NextImage from 'next/legacy/image';
 import type { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { Box, Stack } from '@mui/material';
 
 import type { NextPageWithLayout } from '~/pages/_app';
 import DrawerLayout from '~/components/layouts/drawer-layout';
-import { inAppImages } from '~/constants/image-list';
+
+import dynamic from 'next/dynamic';
+const NextSeo = dynamic(() => import('next-seo').then((mod) => mod.NextSeo));
+const Dashboard = dynamic(() => import('~/components/dashboard-page'));
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
@@ -20,11 +20,7 @@ const DashboardPage: NextPageWithLayout = () => {
   return (
     <>
       <NextSeo title='Dashboard' />
-      <Stack justifyContent='center' flexGrow={1}>
-        <Box position='relative' width='60%' sx={{ mx: 'auto' }}>
-          <NextImage layout='responsive' {...inAppImages.MSLogo} />
-        </Box>
-      </Stack>
+      <Dashboard />
     </>
   );
 };
