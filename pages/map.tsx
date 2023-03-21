7e96dbb2544ea0057c6c49f08117b3a619910fa1
Allen Lin <ht016746@gmail.com>
2023-03-21 22:49:19 +0700

    perf: optimize map page

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,10 +1,12 @@
-import { NextSeo } from 'next-seo';
 import type { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import type { NextPageWithLayout } from '~/pages/_app';
 import DrawerLayout from '~/components/layouts/drawer-layout';
 import { inAppLinks } from '~/constants/side-nav-links';
+
+import dynamic from 'next/dynamic';
+const NextSeo = dynamic(() => import('next-seo').then((mod) => mod.NextSeo));
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   return {
