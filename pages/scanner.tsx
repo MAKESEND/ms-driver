800d0760439d723df2657f4e54c06184889b69ba
Allen Lin <ht016746@gmail.com>
2023-03-21 22:52:54 +0700

    perf: optimize scanner page

--- pages/scanner.tsx
+++ pages/scanner.tsx
@@ -1,10 +1,14 @@
-import { NextSeo } from 'next-seo';
 import type { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import type { NextPageWithLayout } from '~/pages/_app';
 import DrawerLayout from '~/components/layouts/drawer-layout';
-import { ScannerPage as Scanner } from '~/components/scanner/scanner-page';
+
+import dynamic from 'next/dynamic';
+const NextSeo = dynamic(() => import('next-seo').then((mod) => mod.NextSeo));
+const Scanner = dynamic(() =>
+  import('~/components/scanner/scanner-page').then((mod) => mod.ScannerPage)
+);
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
