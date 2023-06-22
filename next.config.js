ceea1e40741b936bc75050c16f74808f44e0c56e
allenlin <ht016746@gmail.com>
2023-06-22 21:34:01 +0700

    chore: manage envs

--- next.config.js
+++ next.config.js
@@ -5,27 +5,19 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const APP_ENV = process.env.APP_ENV;
-const isProduction = APP_ENV === 'production';
-const NODE_ENV = process.env.NODE_ENV;
+const {
+  APP_ENV,
+  NODE_ENV,
+  MS_DRIVER_MGMT,
+  MS_UPDATE_PARCEL_STATUS,
+  NEXTAUTH_URL,
+  NEXTAUTH_SECRET,
+  PORT,
+  API
+} = require('./config');;
 
 const IS_DEMO = process.env.IS_DEMO === 'true';
-const MS_DRIVER_MGMT = process.env.MS_DRIVER_MGMT;
-const MS_UPDATE_PARCEL_STATUS = process.env.MS_UPDATE_PARCEL_STATUS;
 
-const NEXTAUTH_URL = process.env.NEXTAUTH_URL;
-const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
-
-const PORT = process.env.PORT;
-
-const {
-  MS_API,
-  MS_DEV_API,
-  MS_MERCHANT_API,
-  MS_MERCHANT_DEV_API,
-  MS_LEGACY_API,
-  MS_LEGACY_DEV_API
-} = process.env;
 
 const nextConfig = {
   experimental: {
@@ -40,10 +32,6 @@ const nextConfig = {
     IS_DEMO,
     NEXTAUTH_URL,
     version,
-    MS_API: isProduction ? MS_API : MS_DEV_API,
-    MS_MERCHANT_API: isProduction ? MS_MERCHANT_API : MS_MERCHANT_DEV_API,
-    MS_LEGACY_API: isProduction ? MS_LEGACY_API : MS_LEGACY_DEV_API,
-
   },
   reactStrictMode: true,
   serverRuntimeConfig: {
@@ -52,6 +40,7 @@ const nextConfig = {
     NEXTAUTH_SECRET,
     NODE_ENV,
     PORT,
+    ...API
   },
   async rewrites () {
     return [
