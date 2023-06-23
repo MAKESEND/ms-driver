4d6de7a34855cf73b0a0046567e48a4382538d83
allenlin90 <ht016746@gmail.com>
2023-06-23 17:14:36 +0700

    test: setup jest config

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,28 @@ const createJestConfig = nextJest({
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
+  collectCoverageFrom: [
+    '**/*.{js,ts,jsx,tsx}',
+    '!**/node_modules/**',
+    '!**/constants/**',
+    '!**/mocks/**',
+    '!**/styles/**',
+    '!**/test/**',
+    '!**/public/**',
+    '!**/*.d.ts',
+    '!**/types/index.ts',
+    '!**/.*/**',
+    '!./*.{js,ts}',
+  ],
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: -20,
+    },
+  },
   moduleDirectories: ['node_modules', '<rootDir>/'],
   moduleNameMapper: {
     '~/(.*)$': '<rootDir>/./$1',
