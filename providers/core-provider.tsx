83818cc2e19d9f02fb615d5857fce54347a64055
allenlin90 <ht016746@gmail.com>
2023-06-24 23:52:31 +0700

    chore: dynamic import recoil

--- providers/core-provider.tsx
+++ providers/core-provider.tsx
@@ -1,7 +1,11 @@
-import { RecoilRoot } from 'recoil';
 import { StyleProvider } from './style-provider';
 import { ModalProvider } from './modal-provider';
 import { ToastProvider } from './toast-provider';
+
+import dynamic from 'next/dynamic';
+const RecoilRoot = dynamic(() =>
+  import('recoil').then((mod) => mod.RecoilRoot)
+);
 
 const CoreProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
